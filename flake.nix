6c13f43aef029a8a5c64898844ae81b2a04adf5c
Pablo <pabloism0x@gmail.com>
2026-06-25 18:20:51 +0900

    build: provide a reproducible nix dev environment (#160)

---
+++ flake.nix
@@ -0,0 +1,44 @@
+{
+  description = "kwokka - a completion-based async runtime for Rust";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      rust-overlay,
+      flake-utils,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [ (import rust-overlay) ];
+        };
+        toolchain = pkgs.rust-bin.fromRustupToolchainFile ./rust-toolchain.toml;
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          packages = [
+            toolchain
+            pkgs.cargo-nextest
+            pkgs.cargo-llvm-cov
+            pkgs.cargo-deny
+            pkgs.taplo
+            pkgs.typos
+          ];
+        };
+
+        formatter = pkgs.nixfmt;
+      }
+    );
+}
